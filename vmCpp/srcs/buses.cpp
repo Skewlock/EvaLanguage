916c0be7454862dca5c1ea885f2f66eb26b29fbc
Skewlock <Skewlock@gmail.com>
2022-04-13 08:46:57 +0200

    Added comments

--- vmCpp/srcs/buses.cpp
+++ vmCpp/srcs/buses.cpp
@@ -1,5 +1,19 @@
+/**
+ * @file buses.cpp
+ * @author Mathias Couriol (mathias.couriol@gmail.com)
+ * @brief Buses to link the CPU and the RAM or other devices
+ * @version 0.1
+ * @date 2022-04-12
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
 #include "../include/buses.hpp"
 
+/**
+ * @brief Construct a new Buses:: Buses object
+ * 
+ */
 Buses::Buses()
 {
     this->addressBus = 0;
@@ -7,31 +21,61 @@ Buses::Buses()
     this->readWritePin = READ;
 }
 
+/**
+ * @brief return the data bus 
+ * 
+ * @return uint64 
+ */
 uint64 Buses::getDataBus(void)
 {
     return (this->dataBus);
 }
 
+/**
+ * @brief return the address bus
+ * 
+ * @return uint32 
+ */
 uint32 Buses::getAddressBus(void)
 {
     return (this->addressBus);
 }
 
+/**
+ * @brief return the Read/Write pin
+ * 
+ * @return uint8 
+ */
 uint8 Buses::getReadWritePin(void)
 {
     return (this->readWritePin);
 }
 
+/**
+ * @brief Set the data bus
+ * 
+ * @param data 
+ */
 void Buses::setDataBus(uint64 data)
 {
     this->dataBus = data;
 }
 
+/**
+ * @brief Set the address bus
+ * 
+ * @param address 
+ */
 void Buses::setAddressBus(uint32 address)
 {
     this->addressBus = address;
 }
 
+/**
+ * @brief Set the Read/Write pin
+ * 
+ * @param pin 
+ */
 void Buses::setReadWritePin(uint8 pin)
 {
     this->readWritePin= pin;
--- vmCpp/srcs/clock.cpp
+++ vmCpp/srcs/clock.cpp
@@ -1,17 +1,40 @@
+/**
+ * @file clock.cpp
+ * @author Mathias Couriol (mathias.couriol@gmail.com)
+ * @brief the clock
+ * @version 0.1
+ * @date 2022-04-13
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
 #include "../include/clock.hpp"
 
+/**
+ * @brief Construct a new Clock:: Clock object
+ * 
+ * @param speed 
+ */
 Clock::Clock(int speed)
 {
     this->speed = speed;
     this->state = 0;
 }
 
+/**
+ * @brief Construct a new Clock:: Clock object
+ * 
+ */
 Clock::Clock()
 {
     this->speed = 100;
     this->state = 0;
 }
 
+/**
+ * @brief advance the clock (make it tick)
+ * 
+ */
 void Clock::tick(void)
 {
     this->state = (this->state == 0);
