2cd30a903954248811634dd2861413439ed9220f
Skewlock <Skewlock@gmail.com>
2022-05-20 08:31:26 +0200

    Adapting architecture to its updated form

--- vmCpp/include/main.hpp
+++ vmCpp/include/main.hpp
@@ -8,9 +8,13 @@
 #define RAM_MAX 4294967295
 
 //defining general types
-typedef unsigned char uint8; // byte
-typedef unsigned int uint32; // max RAM length
-typedef unsigned long int uint64; // length of instruction
+typedef unsigned char uint8;
+typedef unsigned int uint32;
+typedef struct uint48
+{
+    unsigned long int bits : 48;
+} uint48;
+typedef unsigned long int uint64;
 
 #include "buses.hpp"
 #include "clock.hpp"
