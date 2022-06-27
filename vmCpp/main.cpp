9973a4e129f27fe2d90d514f37e303a425774f7c
Skewlock <Skewlock@gmail.com>
2022-06-27 16:59:43 +0200

    Added flags

--- vmCpp/main.cpp
+++ vmCpp/main.cpp
@@ -9,6 +9,18 @@
  * 
  */
 #include "include/main.hpp"
+#include <sstream>
+#include <vector>
+
+std::vector<std::string> split(std::string s, char del = ' ')
+{
+    std::vector<std::string> res;
+    std::string tok;
+    std::stringstream stream(s);
+    while(std::getline(stream, tok, del))
+        res.push_back(tok);
+    return res;
+}
 
 /**
  * @brief the main to run
@@ -21,20 +33,64 @@ int main(int argc, char *argv[])
 {
     uint32 ram_size;
     VirtualMachine *vm;
+    std::string filename;
+    std::vector<std::string> flag;
 
     // checking correct args size
-    if (argc < 2)
-        ram_size = RAM_MIN;
-    else 
+    switch(argc)
     {
-        long int temp = atoi(argv[1]);
-        if (temp < RAM_MIN || temp > RAM_MAX)
-        {
-            std::cerr << "Invalid RAM size." << std::endl;
-            return (-1);
-        }
-        ram_size = (uint32) temp;
-    }
+        case 1:
+            ram_size = RAM_MIN;
+            filename = "memory.mem";
+            break;
+        case 2:
+            flag = split(std::string(argv[1]), '=');
+            if (flag.front() == "-ramSize")
+            {
+                long int temp = atoi(flag.back().c_str());
+                if (temp < RAM_MIN || temp > RAM_MAX)
+                {
+                    std::cerr << "Invalid RAM size." << std::endl;
+                    return (-1);
+                }
+                ram_size = (uint32) temp;
+                filename = "memory.mem";
+            }
+            else if (flag.front() == "-fileName")
+            {
+                filename = flag.back();
+                ram_size = RAM_MIN;
+            }
+            break;
+
+        case 3:
+            flag = split(std::string(argv[1]), '=');
+            if (flag.front() == "-ramSize")
+            {
+                long int temp = atoi(flag.back().c_str());
+                if (temp < RAM_MIN || temp > RAM_MAX)
+                {
+                    std::cerr << "Invalid RAM size." << std::endl;
+                    return (-1);
+                }
+                ram_size = (uint32) temp;
+                flag = split(std::string(argv[2]), '=');
+                filename = flag.back();
+            }
+            else if (flag.front() == "-fileName")
+            {
+                filename = flag.back();
+                flag = split(std::string(argv[2]), '=');
+                long int temp = atoi(flag.back().c_str());
+                if (temp < RAM_MIN || temp > RAM_MAX)
+                {
+                    std::cerr << "Invalid RAM size." << std::endl;
+                    return (-1);
+                }
+                ram_size = (uint32) temp;
+            }
+            break;
+    };
 
     //create the VM with the good RAM size
     vm = new VirtualMachine(ram_size);
